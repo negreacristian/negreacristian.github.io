473e04b51bc50316247f69306b72d99899f346f1
negreacristian <negreacristi22@gmail.com>
2024-10-08 13:59:33 +0300

    Changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//import Header from './components/Header';
-//import Footer from './components/Footer';
-//import Home from './pages/Home';
-//import About from './pages/About';
-//import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact.js';
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Header />
-      <Switch>
+     
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={Projects} />
         <Route path="/contact" component={Contact} />
-      </Switch>
+
       <Footer />
     </div>
   </Router>
