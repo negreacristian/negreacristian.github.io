33603794d63fc63cb24b2d08111616b42fa26929
negreacristian <negreacristi22@gmail.com>
2024-10-08 13:51:13 +0300

    C1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
+//import Header from './components/Header';
+//import Footer from './components/Footer';
+//import Home from './pages/Home';
+//import About from './pages/About';
+//import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
 function App() {
